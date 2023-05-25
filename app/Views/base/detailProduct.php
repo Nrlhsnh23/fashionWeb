ee4a46c290f621ef276059ec706d196be85cf274
MutiaraArtama <mutiaratama.azza@gmail.com>
2023-05-25 11:21:11 +0700

    versi baru

--- app/Views/base/detailProduct.php
+++ app/Views/base/detailProduct.php
@@ -26,7 +26,6 @@
                 <a href="/" class="btn btn-primary"> Back to home </a>
                 </div> 
 
-                
                 <div class="col-lg-12 mt-5">
                 <a href="/" class="btn btn-primary"> Back to home </a>
                 </div> 
